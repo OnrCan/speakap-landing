1ea3c88f68b00a750295a4b9c71dda8668036933
Onur Can <onrcan.ozkan@gmail.com>
2020-09-24 01:05:54 +0300

    minor updates

--- dist/main.js
+++ dist/main.js
@@ -1,7 +1,1 @@
 "use strict";
-
-var foo = ['a', 'b', 'c'];
-var bar = [].concat(foo).map(function (el) {
-  return el.toUpperCase();
-});
-console.log(bar);
--- js/main.js
+++
@@ -1,5 +0,0 @@
-const foo = ['a', 'b', 'c'];
-
-let bar = [...foo].map(el => el.toUpperCase());
-
-console.log(bar);
